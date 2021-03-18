49184768de91b2c3d117a799a61906a9e81c2a17
Samuel Girardin <samuelgirardin@pm.me>
2021-03-18 15:20:12 +0100

    add possibility to get raw changes object

--- moderation/diff.py
+++ moderation/diff.py
@@ -10,10 +10,9 @@ from django.utils.html import escape
 
 
 class BaseChange(object):
-
     def __repr__(self):
         value1, value2 = self.change
-        return 'Change object: {} - {}'.format(value1, value2)
+        return "Change object: {} - {}".format(value1, value2)
 
     def __init__(self, verbose_name, field, change):
         self.verbose_name = verbose_name
@@ -27,7 +26,6 @@ class BaseChange(object):
 
 
 class TextChange(BaseChange):
-
     @property
     def diff(self):
         value1, value2 = escape(self.change[0]), escape(self.change[1])
@@ -35,21 +33,22 @@ class TextChange(BaseChange):
             return value1
 
         return self.render_diff(
-            'moderation/html_diff.html',
-            {'diff_operations': get_diff_operations(*self.change)})
+            "moderation/html_diff.html",
+            {"diff_operations": get_diff_operations(*self.change)},
+        )
 
 
 class ImageChange(BaseChange):
-
     @property
     def diff(self):
         left_image, right_image = self.change
         return self.render_diff(
-            'moderation/image_diff.html',
-            {'left_image': left_image, 'right_image': right_image})
+            "moderation/image_diff.html",
+            {"left_image": left_image, "right_image": right_image},
+        )
 
 
-def get_change(model1, model2, field, resolve_foreignkeys=False):
+def get_change(model1, model2, field, resolve_foreignkeys=False, raw=False):
     try:
         value1 = getattr(model1, "get_%s_display" % field.name)()
         value2 = getattr(model2, "get_%s_display" % field.name)()
@@ -60,18 +59,24 @@ def get_change(model1, model2, field, resolve_foreignkeys=False):
         else:
             value1 = field.value_from_object(model1)
             value2 = field.value_from_object(model2)
-
-    change = get_change_for_type(
-        field.verbose_name,
-        (value1, value2),
-        field,
-    )
-
+    if raw:
+        change = get_raw_change_for_type(
+            field.verbose_name,
+            (value1, value2),
+            field,
+        )
+    else:
+        change = get_change_for_type(
+            field.verbose_name,
+            (value1, value2),
+            field,
+        )
     return change
 
 
-def get_changes_between_models(model1, model2, excludes=None, includes=None,
-                               resolve_foreignkeys=False):
+def get_changes_between_models(
+    model1, model2, excludes=None, includes=None, resolve_foreignkeys=False, raw=False
+):
     changes = {}
 
     if excludes is None:
@@ -89,46 +94,45 @@ def get_changes_between_models(model1, model2, excludes=None, includes=None,
                 continue
 
             name = "{}__{}".format(model1.__class__.__name__.lower(), field.name)
-
-            changes[name] = get_change(model1, model2, field,
-                                       resolve_foreignkeys)
+            changes[name] = get_change(model1, model2, field, resolve_foreignkeys, raw)
 
     return changes
 
 
 def get_diff_operations(a, b):
     operations = []
-    a_words = re.split(r'(\W+)', a)
-    b_words = re.split(r'(\W+)', b)
+    a_words = re.split(r"(\W+)", a)
+    b_words = re.split(r"(\W+)", b)
     sequence_matcher = difflib.SequenceMatcher(None, a_words, b_words)
     for opcode in sequence_matcher.get_opcodes():
         operation, start_a, end_a, start_b, end_b = opcode
 
-        deleted = ''.join(a_words[start_a:end_a])
-        inserted = ''.join(b_words[start_b:end_b])
+        deleted = "".join(a_words[start_a:end_a])
+        inserted = "".join(b_words[start_b:end_b])
 
-        operations.append({'operation': operation,
-                           'deleted': deleted,
-                           'inserted': inserted})
+        operations.append(
+            {"operation": operation, "deleted": deleted, "inserted": inserted}
+        )
     return operations
 
 
 def html_to_list(html):
-    pattern = re.compile(r'&.*?;|(?:<[^<]*?>)|'
-                         r'(?:\w[\w-]*[ ]*)|(?:<[^<]*?>)|'
-                         r'(?:\s*[,\.\?]*)', re.UNICODE)
+    pattern = re.compile(
+        r"&.*?;|(?:<[^<]*?>)|" r"(?:\w[\w-]*[ ]*)|(?:<[^<]*?>)|" r"(?:\s*[,\.\?]*)",
+        re.UNICODE,
+    )
 
-    return [''.join(element) for element in
-            [_f for _f in pattern.findall(html) if _f]]
+    return ["".join(element) for element in [_f for _f in pattern.findall(html) if _f]]
 
 
 def get_change_for_type(verbose_name, change, field):
     if isinstance(field, fields.files.ImageField):
         change = ImageChange(
             "Current %(verbose_name)s / "
-            "New %(verbose_name)s" % {'verbose_name': verbose_name},
+            "New %(verbose_name)s" % {"verbose_name": verbose_name},
             field,
-            change)
+            change,
+        )
     else:
         value1, value2 = change
         change = TextChange(
@@ -137,4 +141,14 @@ def get_change_for_type(verbose_name, change, field):
             (str(value1), str(value2)),
         )
 
+    return change
+
+
+def get_raw_change_for_type(verbose_name, change, field):
+    value1, value2 = change
+    change = BaseChange(
+        verbose_name,
+        field,
+        (value1, value2),
+    )
     return change
